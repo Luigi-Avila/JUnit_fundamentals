03294b8a65c184572e615491fad3922a070d8730
Luigi <l.avila.uvg@gmail.com>
2023-10-09 11:34:46 -0400

    feat: data class and assertions created

---
+++ app/src/main/java/com/luigidev/junitfundamentals/Assertions.kt
@@ -0,0 +1,28 @@
+package com.luigidev.junitfundamentals
+
+class Assertions {
+    private val user = User("Lugi", 23)
+
+    fun getLuckyNumber(): Array<Int>{
+        return arrayOf(21,117)
+    }
+
+    fun getName(): String{
+        return user.name
+    }
+
+    fun checkHuman(user: User): Boolean{
+        return user.isHuman
+    }
+
+    fun checkHuman(user: User? = null): Boolean?{
+        if (user == null) return null
+        return user.isHuman
+    }
+
+    fun isAdult(user: User): Boolean{
+        if (!user.isHuman) return true
+        return user.age >= 18
+    }
+
+}
---
+++ app/src/main/java/com/luigidev/junitfundamentals/User.kt
@@ -0,0 +1,7 @@
+package com.luigidev.junitfundamentals
+
+data class User(
+    var name: String = "",
+    var age: Int = 0,
+    var isHuman: Boolean = true
+)
