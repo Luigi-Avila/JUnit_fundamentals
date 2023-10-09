e161a761598a9adbb820c256f5e0a47ba7cda1c3
Luigi <l.avila.uvg@gmail.com>
2023-10-09 12:32:49 -0400

    feat: check human test added with assert false and assert true methods

--- app/src/test/java/com/luigidev/junitfundamentals/AssertionsTest.kt
+++ app/src/test/java/com/luigidev/junitfundamentals/AssertionsTest.kt
@@ -3,23 +3,32 @@ package com.luigidev.junitfundamentals
 import org.junit.Assert.*
 import org.junit.Test
 
-class AssertionsTest{
+class AssertionsTest {
 
     @Test
-    fun getLuckyNumberTest(){
+    fun getLuckyNumberTest() {
         val assertions = Assertions()
         val array = arrayOf(21, 117) // Expected values
         //val array = arrayOf(21, 117) // run this to view the custom message
-        assertArrayEquals("Custom message for testing ",array, assertions.getLuckyNumber())
+        assertArrayEquals("Custom message for testing ", array, assertions.getLuckyNumber())
     }
 
     @Test
-    fun getNameTest(){
+    fun getNameTest() {
         val assertions = Assertions()
-        val name="Luigi"
+        val name = "Luigi"
         val otherName = "Luis"
         assertEquals(name, assertions.getName())
         //this is for values not equals
         assertNotEquals(otherName, assertions.getName())
+    }
+
+    @Test
+    fun checkHumanTest() {
+        val assertions = Assertions()
+        val bot = User("8bits", 1, false)
+        val juan = User("Juan", 18, true)
+        assertFalse(assertions.checkHuman(bot))
+        assertTrue(assertions.checkHuman(juan))
     }
 }
