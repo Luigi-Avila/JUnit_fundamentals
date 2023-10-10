6bfa2b202bce3de0323d53377c29040d518c81dc
Luigi <l.avila.uvg@gmail.com>
2023-10-10 10:43:08 -0400

    feat: before class and after annotation added

--- app/src/test/java/com/luigidev/junitfundamentals/AssertionsUsersTest.kt
+++ app/src/test/java/com/luigidev/junitfundamentals/AssertionsUsersTest.kt
@@ -1,15 +1,38 @@
 package com.luigidev.junitfundamentals
 
 import org.junit.After
+import org.junit.AfterClass
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.BeforeClass
 
 import org.junit.Test
 
 class AssertionsUsersTest {
 
     private lateinit var bot: User
-    private lateinit var luigi: User
+
+    companion object {
+        private lateinit var luigi: User
+        @BeforeClass @JvmStatic
+        fun setupCommon() {
+            luigi = User("Luigi", 18, true)
+            println("BeforeClass")
+        }
+
+        // In this case we set Luigi because it is used in all tests but
+        // BeforeClass annotation is useful when we need to set up a database or heavy objects
+        // For each test, JUnit runs before and after but this time is at the end of all
+        // The flow is like this
+        // BeforeClass/  Before/Test/After   Before/Test/After /AfterClass
+
+        @AfterClass @JvmStatic
+        fun tearDownCommon(){
+            luigi = User()
+            println("AfterClass")
+        }
+    }
+
 
     @Before
     fun setup() {
@@ -17,7 +40,8 @@ class AssertionsUsersTest {
         luigi = User("Luigi", 18, true)
         println("Before")
     }
-    // For each test, JUnit run before and after
+
+    // For each test, JUnit runs before and after
     // The flow is like this
     // Before/Test/After   Before/Test/After
 
@@ -40,5 +64,20 @@ class AssertionsUsersTest {
     fun checkNotSameUsersTest() {
         assertNotSame(bot, luigi)
         println("Not the same")
+    }
+
+    @Test
+    fun checkSameUsersTest(){
+        val copyLuigi = luigi
+        //This validate the object's reference
+        assertSame(copyLuigi, luigi)
+        println("checkSameUsers")
+    }
+
+    @Test
+    fun checkNotNullUserTest(){
+        val assertions = Assertions()
+        assertNotNull(assertions.checkHuman(luigi))
+        println("checkNotNullUser")
     }
 }
